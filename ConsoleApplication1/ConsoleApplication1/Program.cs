0545afaf42b16f74ab88ac97cd1c73420f1b6753
alpha11110 <nrw20@case.edu>
2013-09-01 17:33:56 -0400

    test

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaHA_Simulator
+{
+    class MLB
+    {
+        int PC = 0;
+        int inst_count = 0;
+        int[,] SchedTable = new int[128, 2];
+        int[,] RegFile = new int[32,8];
+        //No Operation
+        void NOP()
+        {
+            inst_count++;
+            PC++;
+        }
+        //Add, subtract, add immediate, subtract immediate
+        int ADD(bool[] func, bool[] cond, int a, int b)
+        {
+            int d;
+            if (func[1] == false)
+            {
+                d = a + b;
+            }
+            else
+            {
+                d = a - b;
+            }
+            inst_count++;
+            PC++;
+            return d;
+        }
+        //Shift or rotate
+        int SHF(bool[] func, int a, int b)
+        {
+            int d;
+            //Shift left
+            if (func[1] == false && func[0] == false)
+            {
+                d = a << b;
+            }
+            //Shift right
+            else if (func[1] == false && func[0] == true)
+            {
+                d = a >> b;
+            }
+            //Rotate left
+            else if (func[1] == true && func[0] == false)
+            {
+
+            }
+            //Rotate right
+            else if (func[1] == true && func[0] == true)
+            {
+
+            }
+            inst_count++;
+            PC++;
+            return d;
+        }
+        //Logical operations
+        int LOG(bool[] func, bool[] cond, int a, int b)
+        {
+            int d = 0;
+            //00 = NOT
+            if (func[2] == false && func[1] == false)
+            {
+                d = ~a;
+            }
+            //01 = XOR
+            else if (func[2] == false && func[1] == true)
+            {
+                d = a ^ b;    
+            }
+            //10 = OR
+            else if (func[2] == true && func[1] == false)
+            {
+                d = a | b;
+            }
+            //11 = AND
+            else if (func[2] == true && func[1] == true)
+            {
+                d = a & b;
+            }
+            inst_count++;
+            PC++;
+            return d;
+        }
+        //Lookup table
+        int LUT(bool[] func, bool[] imm, int a)
+        {
+            int d;
+
+            inst_count++;
+            PC++;
+            return d;
+        }
+        //Load or store
+        int LS(bool[] func, bool[] imm, int a)
+        {
+            int d;
+
+            inst_count++;
+            PC++;
+            return d;
+        }
+        //Move
+        int MOV(bool[] func, bool[] imm)
+        {
+            int d;
+
+            inst_count++;
+            PC++;
+            return d;
+        }
+        //Select
+        int SEL(bool[] func, bool[] cond, int a, int b, int c)
+        {
+            int d;
+            if (cond[0] == true)
+            {
+                d = a;
+            }
+            else if (cond[1] == true)
+            {
+                d = b;
+            }
+            else
+            {
+                d = c;
+            }
+            inst_count++;
+            PC++;
+            return d;
+        }
+        //Branch
+        void BR(bool[] func, bool[] cond, int a, int b)
+        {
+
+            inst_count++;
+        }
+        //Fused logic
+        int FUSE(bool[] func, bool[] cond, int a, int b, int c)
+        {
+            int d;
+
+            inst_count++;
+            PC++;
+            return d;
+        }
+        //SIMD XOR
+        int[] QXOR(int[] a, int[] b)
+        {
+            int[] d = new int[4];
+            d[0] = a[0] ^ b[0];
+            d[1] = a[1] ^ b[1];
+            d[2] = a[2] ^ b[2];
+            d[3] = a[3] ^ b[3];
+            inst_count++;
+            PC++;
+            return d;
+        }
+        //SIMD Add
+        int[] QADD(int[] a, int[] b)
+        {
+            int[] d = new int[4];
+            d[0] = a[0] + b[0];
+            d[1] = a[1] + b[1];
+            d[2] = a[2] + b[2];
+            d[3] = a[3] + b[3];
+            inst_count++;
+            PC++;
+            return d;
+        }
+        //SIMD Lookup Table
+        int[] QLUT(int[] a, int[] b)
+        {
+            int[] d = new int[4];
+
+            PC++;
+            return d;
+        }
+
+    }
+}
---
+++ PracticeMLBProject/PracticeMLBProject/PracticeMLBProject/Program.cs
@@ -0,0 +1,330 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//This next mess of code does the same thing as a union in c
+//It basically allows multiple variables inside the structure to map to the same locations in memory
+//Below, picture an integer and an array of four bytes taking up the same memory location
+using System.Runtime.InteropServices;
+[StructLayout(LayoutKind.Explicit)]
+struct ByteInt
+{
+    [FieldOffset(0)]    //The integer starts at the begining offset 0, and takes up 4 bytes
+    public int i;
+    [FieldOffset(0)]    //The first byte also starts at offset 0 and is obviously 1 byte long...
+    public byte b0;
+    [FieldOffset(1)]    //...followed by the second byte at offset 1, and so on. 
+    public byte b1;
+    [FieldOffset(2)]
+    public byte b2;
+    [FieldOffset(3)]
+    public byte b3;
+}
+namespace PracticeMLBProject
+{
+    enum Format {REGISTER=0, IMMEDIATE,BRANCH, SCALAR, SIMD};
+
+    class Program
+    {
+        const int REGS_LENGTH = 32;
+
+        //Since this is just a proof of concept, so I'm lazy, but just close your
+        //eyes and pretend that this is inside the MLB class!
+        static byte[] regs = new byte[REGS_LENGTH];
+
+        static void Main(string[] args)
+        {
+
+            //Create a 32 bit integer
+            int i1 = 1778332123;  //arbitrary value less than 2^32 that still requires all four bytes of the integer
+            Console.Out.WriteLine("\nThe 32 bit value before it gets loaded to the register file is: " + i1);
+
+            //Demonstrate storing and loading all 32 bits
+            StoreToRegs(i1, 3, 4);  // store 4 bytes in r3, r4, r5, and r6
+            int i1Loaded32Bits = LoadFromRegs(3, 4); 
+            Console.Out.WriteLine("\nThe full 32 bit value loaded back from the register file is: " + i1Loaded32Bits);
+
+            //Test loading and storing 24 bits of the 32 bit integer to the register file
+            StoreToRegs(i1, 9, 3);  // store 3 bytes in r9, r10, and r11 
+            int i1Loaded24Bits = LoadFromRegs(9, 3);
+            Console.Out.WriteLine("\nThe first 24 bits loaded back from the register file are: " + i1Loaded24Bits);
+
+            //Test loading and storing 16 bits of the 32 bit integer to the register file
+            StoreToRegs(i1, 13, 2);  // store 2 bytes in r13 and r14
+            int i1Loaded16Bits = LoadFromRegs(13,2);
+            Console.Out.WriteLine("\nThe first 16 bits loaded back from the register file are: " + i1Loaded16Bits);
+
+            //Test loading and storing 16 bits of the 32 bit integer to the register file
+            StoreToRegs(i1, 21, 1);  // store 1 byte in r21
+            int i1Loaded8Bits = LoadFromRegs(21, 1);
+            Console.Out.WriteLine("\nThe first 8 bits loaded back from the register file are: " + i1Loaded8Bits);
+
+            //Print the Register Contents
+            PrintRegs();
+
+
+            Cluster clust = new Cluster();
+            clust.CreateCluster();
+        }
+
+        //StoreInt32ToRegs(): stores an integer value to an sequence of 4 bytes at an arbitrary register location
+        static void StoreToRegs(int intIn, int byteAddress, int widthBytes)
+        {
+            //Initialize the byte converter struct
+            ByteInt converter;
+            converter.b0 = 0;
+            converter.b1 = 0;
+            converter.b2 = 0;
+            converter.b3 = 0;
+
+            //store value in the struct as an int 
+            converter.i = intIn;
+
+            //If the byte address is valid (given the width in bytes) write the data to the reg file
+            if (byteAddress <= REGS_LENGTH - widthBytes)
+            {
+                regs[byteAddress] = converter.b0;   //always write atleast 1 byte
+                //write the second, third, and fourth bytes if the width is two, 3, and 4, respectively
+                if (widthBytes > 1)
+                    regs[byteAddress + 1] = converter.b1;
+                if (widthBytes > 2)
+                    regs[byteAddress + 2] = converter.b2;
+                if (widthBytes > 3)
+                    regs[byteAddress + 3] = converter.b3;
+            }
+        }
+
+        //LoadInt32FromRegs(): Loads an integer from a sequence of four consecutive bytes in the register file
+        static int LoadFromRegs(int byteAddress, int widthBytes)
+        {
+            ByteInt converter;
+            converter.i = 0;
+
+            if (byteAddress <= REGS_LENGTH - widthBytes)  //grab the individual bytes from the reg array and pack them into the struct
+            {
+                converter.b0 = regs[byteAddress];   //always grab at least one byte
+                //grab the second, third and fourth bytes if the width in bytes is 2,3,and 4 respectively
+                if (widthBytes > 1)
+                    converter.b1 = regs[byteAddress + 1];
+                if (widthBytes > 2)
+                    converter.b2 = regs[byteAddress + 2];
+                if (widthBytes > 3)
+                    converter.b3 = regs[byteAddress + 3];
+            }
+
+            return converter.i;  //return the data packed into the struct as an integer
+        }
+
+        //PrintRegs(): Prints the register file to prove that I'm actually not lying!
+        static void PrintRegs()
+        {
+            Console.Out.WriteLine("\nRegister File Contents:");
+            for (int i = 0; i < REGS_LENGTH; i++)
+            {
+                string line = "\tReg[" + i + "]: " + regs[i];
+                Console.Out.WriteLine(line);
+            }
+
+        }
+    }
+
+    //MLB class skeleton  
+    class MLB
+    {
+        //Othere MLBS in the cluster 
+        MLB mlbPlus1;
+        MLB mlbPlus2;
+        MLB mlbPlus3;
+        int mlbClusterIndex;
+
+        //32 byte register file
+        const int REGS_LENGTH = 32;
+        static byte[] Regs = new byte[REGS_LENGTH];
+
+        //"Scalar" single bit register file
+        const int SCAL_REGS_LENGTH = 16;
+        static bool[] scalarRegs = new bool[SCAL_REGS_LENGTH];
+
+        //Pocessor Status Register Bits
+        bool UserSelected;
+        bool CarryOut;
+        bool Overflow;
+        bool Zero;
+
+        //system variables
+        int PC;
+
+        //RegisterClusterLocation():  method called while constructing a cluster to
+        //link an mlb to each of its neighbors in the cluster. This will greatly facilitate 
+        //the implementation of the intercluster bus, since each mlb will simply know what its neighbors are
+        public void RegisterClusterLocation(MLB mlbPlus1, MLB mlbPlus2, MLB mlbPlus3, int mlbClusterIndex)
+        {
+            this.mlbPlus1 = mlbPlus1;
+            this.mlbPlus2 = mlbPlus2;
+            this.mlbPlus3 = mlbPlus3;
+            this.mlbClusterIndex = mlbClusterIndex;
+
+        }
+
+        //ProcessInstruction(): 
+        //Gets the next instruct
+        public void ProcessInstruction()
+        {
+            //MAHA parser would live inside here
+        }
+
+        //Here is where the individual isa instructions could go
+        public void ADD(RegisterFormat rFmt)
+        {
+            
+
+        }
+
+        //StoreInt32ToRegs(): stores an integer value to an sequence of 4 bytes at an arbitrary register location
+        static void StoreToRegs(int intIn, int byteAddress, int widthBytes)
+        {
+            //Initialize the byte converter struct
+            ByteInt converter;
+            converter.b0 = 0;
+            converter.b1 = 0;
+            converter.b2 = 0;
+            converter.b3 = 0;
+
+            //store value in the struct as an int 
+            converter.i = intIn;
+
+            //If the byte address is valid (given the width in bytes) write the data to the reg file
+            if (byteAddress <= REGS_LENGTH - widthBytes)
+            {
+                Regs[byteAddress] = converter.b0;   //always write atleast 1 byte
+                //write the second, third, and fourth bytes if the width is two, 3, and 4, respectively
+                if (widthBytes > 1)
+                    Regs[byteAddress + 1] = converter.b1;
+                if (widthBytes > 2)
+                    Regs[byteAddress + 2] = converter.b2;
+                if (widthBytes > 3)
+                    Regs[byteAddress + 3] = converter.b3;
+            }
+        }
+
+        //LoadInt32FromRegs(): Loads an integer from a sequence of four consecutive bytes in the register file
+        static int LoadFromRegs(int byteAddress, int widthBytes)
+        {
+            ByteInt converter;
+            converter.i = 0;
+
+            if (byteAddress <= REGS_LENGTH - widthBytes)  //grab the individual bytes from the reg array and pack them into the struct
+            {
+                converter.b0 = Regs[byteAddress];   //always grab at least one byte
+                //grab the second, third and fourth bytes if the width in bytes is 2,3,and 4 respectively
+                if (widthBytes > 1)
+                    converter.b1 = Regs[byteAddress + 1];
+                if (widthBytes > 2)
+                    converter.b2 = Regs[byteAddress + 2];
+                if (widthBytes > 3)
+                    converter.b3 = Regs[byteAddress + 3];
+            }
+
+            return converter.i;  //return the data packed into the struct as an integer
+        }
+        //PrintRegs(): Prints the register file to prove that I'm actually not lying!
+        static void PrintRegs()
+        {
+            Console.Out.WriteLine("\nRegister File Contents:");
+            for (int i = 0; i < REGS_LENGTH; i++)
+            {
+                string line = "\tReg[" + i + "]: " + Regs[i];
+                Console.Out.WriteLine(line);
+            }
+
+        }
+    }
+
+    //Cluster: represets a cluster of 4 MLBS
+    class Cluster
+    {
+        MLB[] mlbs = new MLB[4];
+
+        // Create Cluster(): Creates a cluster by instantiating the mlbs and linking them together. Note that
+        //They links are addressed using modulo to allow the neighbor relationships to wrap around (As suggested in the ISA)
+        public void CreateCluster()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                mlbs[i] = new MLB();
+                mlbs[i].RegisterClusterLocation(mlbs[(i + 1) % 4], mlbs[(i + 2) % 4], mlbs[(i + 3) % 4], i); //links each mlb to each of the other mlbs
+            }
+            
+            Instruction inst = new Instruction();
+            
+
+        }
+    }
+
+    //Create an array of Instructions at startup.  Each of these instructions is actually a child object.  Use 
+    //the format field to detemine what type of child it is.  Then cast it to the correct format so that you can extract the fields.
+    //I believe that preparsing the instructions and storing them in such a structure would greatly improve the efficiency of the
+    //simulator. There are more elegant ways of doing this than using the base/child class approach and casts, but this is 
+    //probably the simplest
+
+    //Note: The opcode and format fields are protected because they are critical in determining what the instruction is
+    //Fields in child classes, which contain contain other less critical fields, are generally not private
+  
+    class Instruction
+    {
+
+        protected Format format;
+        protected int opCode;
+        public int OpCode
+        {
+            get { return opCode; }
+        }
+        public Format Format
+        {
+            get { return format; }
+        }
+    }
+    class RegisterFormat : Instruction
+    {
+        public bool[] Func;
+        public int Rd;
+        public int Ra;
+        public int Rb;
+        int Rc;
+        bool[] Cond;
+        RegisterFormat(int opCode, bool[] Func, int Rd, int Ra, int Rb, int Rc, bool[] Cond)
+        {
+           //set generic instruction parameters
+            this.opCode = opCode;
+            this.format = Format.REGISTER;
+
+            //set the parameters specific to an Register format instruction
+            this.Func = Func;
+            this.Rd = Rd;
+            this.Ra = Ra;
+            this.Rb = Rb;
+            this.Rc = Rc;
+            this.Cond = Cond;
+        }
+    }
+    class ImmediateFormat : Instruction
+    {
+
+    }
+    class BranchFormat : Instruction
+    {
+
+    }
+    class ScalarFormat : Instruction
+    {
+
+    }
+    class SIMDFormat : Instruction
+    {
+
+    }
+}
